f7751b55109e1c621f8ff821d6b1b0488cd8779b
ice-blokk <bradymonster8@gmail.com>
2023-03-01 15:49:38 -0500

    Patrick's Variable Names

--- src/main/java/frc/robot/subsystems/Grabber.java
+++ src/main/java/frc/robot/subsystems/Grabber.java
@@ -1,35 +1,46 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import edu.wpi.first.wpilibj.DoubleSolenoid;
-// import edu.wpi.first.wpilibj.PneumaticsModuleType;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
-// public class Grabber extends SubsystemBase {
+public class Grabber extends SubsystemBase {
 
 
-//   private final DoubleSolenoid grabber;
+  private final DoubleSolenoid gorillaGripper;
+  private final Solenoid grab, release;
 
   
-//   /** Creates a new Climber. */
-//   public Grabber() {
-//     grabber = new DoubleSolenoid(1, PneumaticsModuleType.CTREPCM, CONSTANTS.GRAB_MODULE_1, CONSTANTS.GRAB_MODULE_2); 
+  /** Creates a new Climber. */
+  public Grabber() {
+    gorillaGripper = new DoubleSolenoid(1, PneumaticsModuleType.REVPH, Constants.Snake.grabberID1, Constants.Snake.grabberID2); 
+    
+    grab = new Solenoid(PneumaticsModuleType.REVPH, Constants.Snake.grabberID1);
+    release = new Solenoid(PneumaticsModuleType.REVPH, Constants.Snake.grabberID2);
 
-//     grabber.set(DoubleSolenoid.Value.kForward);
+    gorillaGripper.set(DoubleSolenoid.Value.kForward);
 
-//   }
+  }
 
-//   public void grab(){
-//     grabber.toggle();
-// 	}
+  public void grab(){
+    gorillaGripper.toggle();
+	}
+
+  public void grabThang(boolean toGrab){
+    release.set(!toGrab);
+    grab.set(toGrab);
+  }
 
 
 
-//   @Override
-//   public void periodic() {
-//     // This method will be called once per scheduler run
-//   }
-// }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
